57ee8d400d4f2c9a64234fd4ac92a404f45411f5
Lev <levdjepko@gmail.com>
2020-05-12 19:35:48 -0700

    Add files via upload

---
+++ ViewController.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace hundredDaysOf
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void UIButton590_TouchUpInside(UIButton sender)
+        {
+            challengeName.Text = "Your challenge for next 100 days is " + textForChallenge.Text;
+            challengeName.Hidden = false;
+        }
+
+        partial void empty(UITextField sender)
+        {
+            textForChallenge.Text = String.Empty;
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,55 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace hundredDaysOf
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel challengeName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton selectChallengeButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textForChallenge { get; set; }
+
+        [Action ("empty:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void empty (UIKit.UITextField sender);
+
+        [Action ("UIButton590_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton590_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (challengeName != null) {
+                challengeName.Dispose ();
+                challengeName = null;
+            }
+
+            if (selectChallengeButton != null) {
+                selectChallengeButton.Dispose ();
+                selectChallengeButton = null;
+            }
+
+            if (textForChallenge != null) {
+                textForChallenge.Dispose ();
+                textForChallenge = null;
+            }
+        }
+    }
+}
