68bbfeafa926c3285214a5f1207b411033051f10
Lev <levdjepko@gmail.com>
2020-05-16 20:59:05 -0700

    Add files via upload

---
+++ Connections/ISqlConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+
+namespace hundredDaysOf.Connections
+{
+    public class ISqlConnection
+    {
+        
+
+        public interface IsqlConnection
+        {
+            SQLiteAsyncConnection connection();
+        }
+
+        public ISqlConnection()
+        {
+            //constructor?
+        }
+    }
+}
---
+++ SQLConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Foundation;
+using hundredDaysOf;
+
+using SQLite;
+using UIKit;
+using hundredDaysOf.Connections.ISqlConnection;
+using static hundredDaysOf.Connections.ISqlConnection;
+
+[assembly: Dependency(typeof(SQLConnection))]
+namespace hundredDaysOf
+{
+    
+      public class SqlConnection : IsqlConnection
+        {
+            public SQLiteAsyncConnection Connection()
+            {
+                var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                var path = Path.Combine(documentsPath, "MySQLite.sqldb");
+
+                return new SQLiteAsyncConnection(path);
+            }
+        }
+    }
